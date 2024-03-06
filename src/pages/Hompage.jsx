c25ddd926033003429f1f946814345ffa1fa37c3
bread <chalida.bread@gmail.com>
2024-03-06 22:35:43 +0700

    init

---
+++ src/pages/Hompage.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function LoginPage() {
+  return (
+    <div>
+      <div className="w-full h-full">
+        <h1 className="text-red">Login test</h1>
+        <div className="">
+          <div>
+            <span>Home</span>
+            <span>Shop</span>
+            <span>Contact Us</span>
+          </div>
+          <div>
+            <span>The Art and Soul Shop</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default LoginPage;
